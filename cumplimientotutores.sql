fd55b4b2a6f4a07996746fee362eb1ca359b9a83
GusDamian <damiangustavoangel@gmail.com>
2024-05-20 11:53:10 -0600

    06 Redesing Data Base

--- cumplimientotutores.sql
+++ cumplimientotutores.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-05-2024 a las 21:22:23
+-- Tiempo de generación: 20-05-2024 a las 19:51:36
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -29,10 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `actividades` (
   `id` int(11) NOT NULL,
-  `Actividad` text NOT NULL,
-  `Asistencia` text NOT NULL,
-  `Tutor` varchar(25) NOT NULL,
-  `Grupo` varchar(2) NOT NULL
+  `Actividad` varchar(50) NOT NULL,
+  `Asistencia` varchar(50) DEFAULT NULL,
+  `Grupo` varchar(2) DEFAULT NULL,
+  `id_tutor` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -43,11 +43,11 @@ CREATE TABLE `actividades` (
 
 CREATE TABLE `alumnos` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(25) NOT NULL,
-  `ecnuesta_baja` binary(1) NOT NULL,
-  `canalizacion` binary(1) NOT NULL,
-  `mat_acredi_extr` int(3) NOT NULL,
-  `Tutor` varchar(25) NOT NULL
+  `nombre` varchar(25) DEFAULT NULL,
+  `encuesta_baja` binary(1) DEFAULT NULL,
+  `canalizacion` binary(1) DEFAULT NULL,
+  `mat_acredi_extr` int(3) DEFAULT NULL,
+  `id_tutor` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -58,11 +58,22 @@ CREATE TABLE `alumnos` (
 
 CREATE TABLE `entregables` (
   `No` int(11) NOT NULL,
-  `Carrera` varchar(7) NOT NULL,
-  `Grupo` varchar(2) NOT NULL,
-  `Tutor` varchar(25) NOT NULL,
+  `Carrera` varchar(7) DEFAULT NULL,
+  `Grupo` varchar(2) DEFAULT NULL,
   `idActividad` int(11) DEFAULT NULL,
-  `idAlumno` int(11) DEFAULT NULL
+  `idAlumno` int(11) DEFAULT NULL,
+  `id_tutor` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tutor`
+--
+
+CREATE TABLE `tutor` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -74,25 +85,29 @@ CREATE TABLE `entregables` (
 --
 ALTER TABLE `actividades`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `Tutor` (`Tutor`),
-  ADD UNIQUE KEY `Grupo` (`Grupo`);
+  ADD KEY `id_tutor` (`id_tutor`);
 
 --
 -- Indices de la tabla `alumnos`
 --
 ALTER TABLE `alumnos`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `Tutor` (`Tutor`);
+  ADD KEY `id_tutor` (`id_tutor`);
 
 --
 -- Indices de la tabla `entregables`
 --
 ALTER TABLE `entregables`
   ADD PRIMARY KEY (`No`),
-  ADD UNIQUE KEY `idActividad` (`idActividad`),
-  ADD UNIQUE KEY `Tutor` (`Tutor`),
-  ADD UNIQUE KEY `idAlumno` (`idAlumno`),
-  ADD UNIQUE KEY `Grupo` (`Grupo`);
+  ADD KEY `idActividad` (`idActividad`),
+  ADD KEY `idAlumno` (`idAlumno`),
+  ADD KEY `id_tutor` (`id_tutor`);
+
+--
+-- Indices de la tabla `tutor`
+--
+ALTER TABLE `tutor`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -102,19 +117,25 @@ ALTER TABLE `entregables`
 -- AUTO_INCREMENT de la tabla `actividades`
 --
 ALTER TABLE `actividades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `alumnos`
 --
 ALTER TABLE `alumnos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `entregables`
 --
 ALTER TABLE `entregables`
-  MODIFY `No` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `No` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `tutor`
+--
+ALTER TABLE `tutor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
@@ -124,21 +145,21 @@ ALTER TABLE `entregables`
 -- Filtros para la tabla `actividades`
 --
 ALTER TABLE `actividades`
-  ADD CONSTRAINT `actividades_ibfk_1` FOREIGN KEY (`Tutor`) REFERENCES `entregables` (`Tutor`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `actividades_ibfk_2` FOREIGN KEY (`Grupo`) REFERENCES `entregables` (`Grupo`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `actividades_ibfk_1` FOREIGN KEY (`id_tutor`) REFERENCES `tutor` (`id`);
 
 --
 -- Filtros para la tabla `alumnos`
 --
 ALTER TABLE `alumnos`
-  ADD CONSTRAINT `alumnos_ibfk_1` FOREIGN KEY (`Tutor`) REFERENCES `entregables` (`Tutor`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `alumnos_ibfk_1` FOREIGN KEY (`id_tutor`) REFERENCES `tutor` (`id`);
 
 --
 -- Filtros para la tabla `entregables`
 --
 ALTER TABLE `entregables`
-  ADD CONSTRAINT `entregables_ibfk_1` FOREIGN KEY (`idActividad`) REFERENCES `actividades` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `entregables_ibfk_2` FOREIGN KEY (`idAlumno`) REFERENCES `alumnos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `entregables_ibfk_1` FOREIGN KEY (`idActividad`) REFERENCES `actividades` (`id`),
+  ADD CONSTRAINT `entregables_ibfk_2` FOREIGN KEY (`idAlumno`) REFERENCES `alumnos` (`id`),
+  ADD CONSTRAINT `entregables_ibfk_3` FOREIGN KEY (`id_tutor`) REFERENCES `tutor` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
